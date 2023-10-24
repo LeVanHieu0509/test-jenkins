0877fd5aacc95178a27589efb489df6e98223009
LeVanHieu0509 <leVanHieu.huflit@gmail.com>
2023-10-24 18:03:49 +0700

    oik

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello ok hieu pro!');
+    res.send('Hello ok hieu pro 123!');
 });
 
 app.listen(port, () => {
